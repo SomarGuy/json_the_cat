507b99a802ad82ef84dc498ea5ba42bd0a085a48
Somaro <106792137+SomarGuy@users.noreply.github.com>
2022-11-22 20:07:00 -0500

    breedFetcherJS Final

---
+++ breedFetcher.js
@@ -0,0 +1,21 @@
+const request = require("request");
+
+const fetchBreedDescription = function(breedName, callback) {
+  request(
+    `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`,
+    (error, res, body) => {
+      if (!error) {
+        let data = JSON.parse(body)[0];
+        if (!data) {
+          callback("Breed does not exist!");
+        } else {
+          callback(null, data.description.trim());
+        }
+      } else {
+        callback(error);
+      }
+    }
+  );
+};
+
+module.exports = { fetchBreedDescription };
